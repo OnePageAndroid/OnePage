0538fdd3dc1c09d6839ef21f22d71d9e9aa3341f
ojh102 <ojh102@gmail.com>
2017-01-04 20:46:30 +0900

    splash

--- app/src/main/java/kr/nexters/onepage/SplashActivity.java
+++ app/src/main/java/kr/nexters/onepage/SplashActivity.java
@@ -1,5 +1,7 @@
 package kr.nexters.onepage;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+            }
+        }, 3000);
     }
 }
