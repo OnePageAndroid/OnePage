2a8da6d7499fb9122823e28c9b8e7ed3b6ecbcd8
Eunong <hoodyong@gmail.com>
2017-01-18 02:01:56 +0900

    add WriteActivity

--- app/src/main/java/kr/nexters/onepage/map/MapActivity.java
+++ app/src/main/java/kr/nexters/onepage/map/MapActivity.java
@@ -64,20 +64,21 @@ public class MapActivity extends AppCompatActivity {
         public void onMapReady(GoogleMap googleMap) {
             mGoogleMap = googleMap;
 
-            //Android version 위치 요청 권한 확인
+            //Check location permission
             checkPermission();
 
             //적절한 위치제공자 선택
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
-            criteria.setAltitudeRequired(false);
+//            Criteria criteria = new Criteria();
+//            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+//            criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+//            criteria.setAltitudeRequired(false);
+//
+//            String bestProvider = locationManager.getBestProvider(criteria, true);
+//            Log.i("bestProvider", bestProvider);
+//            locationManager.requestLocationUpdates(bestProvider, 10000, 3, locationListener);
 
-            String bestProvider = locationManager.getBestProvider(criteria, true);
-            Log.i("bestProvider", bestProvider);
-
-            //10초 간격, 3미터 이상 이동시 update
-            locationManager.requestLocationUpdates(bestProvider, 10000, 3, locationListener);
+            //5초 간격, 3미터 이상 이동시 update
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 5000, 3, locationListener);
 
             //디비에서 받아올 위도, 경도로 Marker 표시 해주기
 
@@ -156,7 +157,7 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //Android version 위치 요청 권한 확인
+        //Check location permission
         checkPermission();
         //위치 정보 수신 종료
         locationManager.removeUpdates(locationListener);
@@ -164,7 +165,7 @@ public class MapActivity extends AppCompatActivity {
 
     //Permission Check
     private void checkPermission() {
-        //퍼미션이 거절됐을 경우 request
+        //If permission is denied, request
         if(ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED
                 && ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED ) {
             ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_LOCATION);
