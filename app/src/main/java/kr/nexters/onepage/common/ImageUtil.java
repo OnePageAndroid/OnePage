e7b442d7a4c6a9fdc40e4facda782257886434de
ojh102 <ojh102@gmail.com>
2017-02-15 10:42:51 +0900

    툴바 그림자 처리

--- app/src/main/java/kr/nexters/onepage/common/ImageUtil.java
+++ app/src/main/java/kr/nexters/onepage/common/ImageUtil.java
@@ -1,10 +1,13 @@
 package kr.nexters.onepage.common;
 
 import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by OhJaeHwan on 2017-02-14.
@@ -22,5 +25,36 @@ public class ImageUtil {
         newCanvas.drawBitmap(bm2, 0, 0, paint);
 
         return newBitmap;
+    }
+
+
+    public static void applyBlurMaskFilter(TextView tv, BlurMaskFilter.Blur style){
+
+        // Define the blur effect radius
+        float radius = tv.getTextSize()/10;
+
+        // Initialize a new BlurMaskFilter instance
+        BlurMaskFilter filter = new BlurMaskFilter(radius,style);
+
+        // Set the TextView layer type
+        tv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        // Finally, apply the blur effect on TextView text
+        tv.getPaint().setMaskFilter(filter);
+    }
+
+    public static void applyBlurMaskFilter(TextView tv, BlurMaskFilter.Blur style, float radius){
+
+        // Define the blur effect radius
+//        float radius = tv.getTextSize()/10;
+
+        // Initialize a new BlurMaskFilter instance
+        BlurMaskFilter filter = new BlurMaskFilter(radius,style);
+
+        // Set the TextView layer type
+        tv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        // Finally, apply the blur effect on TextView text
+        tv.getPaint().setMaskFilter(filter);
     }
 }
